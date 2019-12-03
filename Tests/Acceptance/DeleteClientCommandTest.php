ce2946003b13133c811d71e1e17d0f1859aa0746
Josip Igrec <me@allypost.net>
2019-12-03 15:22:03 +0100

    Move ClientManager related tests from DeleteClientCommandTest to DoctrineClientManagerTest

--- Tests/Acceptance/DeleteClientCommandTest.php
+++ Tests/Acceptance/DeleteClientCommandTest.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace Trikoder\Bundle\OAuth2Bundle\Tests\Acceptance;
 
-use DateTime;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
-use Trikoder\Bundle\OAuth2Bundle\Manager\AccessTokenManagerInterface;
 use Trikoder\Bundle\OAuth2Bundle\Manager\ClientManagerInterface;
-use Trikoder\Bundle\OAuth2Bundle\Manager\RefreshTokenManagerInterface;
-use Trikoder\Bundle\OAuth2Bundle\Model\AccessToken;
 use Trikoder\Bundle\OAuth2Bundle\Model\Client;
-use Trikoder\Bundle\OAuth2Bundle\Model\RefreshToken;
 
 /**
  * @covers \Trikoder\Bundle\OAuth2Bundle\Command\DeleteClientCommand
@@ -37,75 +32,6 @@ final class DeleteClientCommandTest extends AbstractAcceptanceTest
         $this->assertNull($client);
     }
 
-    public function testDeleteClientWithAccessTokens(): void
-    {
-        $client = $this->fakeAClient('foobar');
-        $this->getClientManager()->save($client);
-
-        $accessToken = $this->fakeAnAccessToken(
-            'bazqux',
-            'xyzzy',
-            $client
-        );
-        $this->getAccessTokenManager()->save($accessToken);
-
-        $command = $this->command();
-        $commandTester = new CommandTester($command);
-        $commandTester->execute([
-            'command' => $command->getName(),
-            'identifier' => $client->getIdentifier(),
-        ]);
-
-        $this->clearEM();
-
-        $output = $commandTester->getDisplay();
-        $this->assertStringContainsString('Given oAuth2 client deleted successfully', $output);
-
-        $client = $this->findClient($client->getIdentifier());
-        $this->assertNull($client);
-
-        $accessToken = $this->findAccessToken($accessToken->getIdentifier());
-        $this->assertNull($accessToken);
-    }
-
-    public function testDeleteClientWithAccessAndRefreshTokens(): void
-    {
-        $client = $this->fakeAClient('foobar');
-        $this->getClientManager()->save($client);
-
-        $accessToken = $this->fakeAnAccessToken(
-            'bazqux',
-            'xyzzy',
-            $client
-        );
-        $this->getAccessTokenManager()->save($accessToken);
-
-        $refreshToken = $this->fakeARefreshToken('killroy', $accessToken);
-        $this->getRefreshTokenManager()->save($refreshToken);
-
-        $command = $this->command();
-        $commandTester = new CommandTester($command);
-        $commandTester->execute([
-            'command' => $command->getName(),
-            'identifier' => $client->getIdentifier(),
-        ]);
-
-        $this->clearEM();
-
-        $output = $commandTester->getDisplay();
-        $this->assertStringContainsString('Given oAuth2 client deleted successfully', $output);
-
-        $client = $this->findClient($client->getIdentifier());
-        $this->assertNull($client);
-
-        $accessToken = $this->findAccessToken($accessToken->getIdentifier());
-        $this->assertNull($accessToken);
-
-        $refreshToken = $this->findRefreshToken($refreshToken->getIdentifier());
-        $this->assertNotNull($refreshToken);
-        $this->assertNull($refreshToken->getAccessToken());
-    }
-
     public function testDeleteNonExistentClient(): void
     {
         $identifierName = 'invalid identifier';
@@ -128,52 +54,9 @@ final class DeleteClientCommandTest extends AbstractAcceptanceTest
             ;
     }
 
-    private function findAccessToken(string $identifier): ?AccessToken
-    {
-        return
-            $this
-                ->getAccessTokenManager()
-                ->find($identifier)
-            ;
-    }
-
-    private function findRefreshToken(string $identifier): ?RefreshToken
-    {
-        return
-            $this
-                ->getRefreshTokenManager()
-                ->find($identifier)
-            ;
-    }
-
     private function fakeAClient(string $identifier): Client
     {
         return new Client($identifier, 'quzbaz');
-    }
-
-    private function fakeAnAccessToken(
-        string $identifier,
-        string $userIdentifier,
-        Client $client,
-        array $scopes = [],
-        string $timeModifier = '+1 day'
-    ): AccessToken {
-        return new AccessToken(
-            $identifier,
-            (new DateTime('now'))->modify($timeModifier),
-            $client,
-            $userIdentifier,
-            $scopes
-        );
-    }
-
-    private function fakeARefreshToken(string $identifier, AccessToken $accessToken, string $timeModifier = '+1 day')
-    {
-        return new RefreshToken(
-            $identifier,
-            (new DateTime('now'))->modify($timeModifier),
-            $accessToken
-        );
     }
 
     private function getClientManager(): ClientManagerInterface
@@ -184,37 +67,6 @@ final class DeleteClientCommandTest extends AbstractAcceptanceTest
                 ->getContainer()
                 ->get(ClientManagerInterface::class)
             ;
-    }
-
-    private function getAccessTokenManager(): AccessTokenManagerInterface
-    {
-        return
-            $this
-                ->client
-                ->getContainer()
-                ->get(AccessTokenManagerInterface::class)
-            ;
-    }
-
-    private function getRefreshTokenManager(): RefreshTokenManagerInterface
-    {
-        return
-            $this
-                ->client
-                ->getContainer()
-                ->get(RefreshTokenManagerInterface::class)
-            ;
-    }
-
-    private function clearEM()
-    {
-        $this
-            ->client
-            ->getContainer()
-            ->get('doctrine')
-            ->getManager()
-            ->clear()
-        ;
     }
 
     private function command(): Command
---
+++ Tests/Acceptance/DoctrineClientManagerTest.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trikoder\Bundle\OAuth2Bundle\Tests\Acceptance;
+
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Trikoder\Bundle\OAuth2Bundle\Manager\Doctrine\ClientManager as DoctrineClientManager;
+use Trikoder\Bundle\OAuth2Bundle\Model\AccessToken;
+use Trikoder\Bundle\OAuth2Bundle\Model\Client;
+use Trikoder\Bundle\OAuth2Bundle\Model\RefreshToken;
+
+class DoctrineClientManagerTest extends AbstractAcceptanceTest
+{
+    public function testSimpleDelete(): void
+    {
+        /** @var $em EntityManagerInterface */
+        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $doctrineAccessTokenManager = new DoctrineClientManager($em);
+
+        $client = new Client('client', 'secret');
+        $em->persist($client);
+        $em->flush();
+
+        $doctrineAccessTokenManager->remove($client);
+
+        $this->assertNull(
+            $em
+                ->getRepository(Client::class)
+                ->findOneBy(
+                    [
+                        'identifier' => $client->getIdentifier(),
+                    ]
+                )
+        );
+    }
+
+    public function testClientDeleteCascadesToAccessTokens(): void
+    {
+        /** @var $em EntityManagerInterface */
+        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $doctrineAccessTokenManager = new DoctrineClientManager($em);
+
+        $client = new Client('client', 'secret');
+        $em->persist($client);
+        $em->flush();
+
+        timecop_freeze(new DateTime());
+
+        try {
+            $accessToken = new AccessToken('access token', (new DateTime())->modify('+1 day'), $client, $client->getIdentifier(), []);
+            $em->persist($accessToken);
+            $em->flush();
+
+            $doctrineAccessTokenManager->remove($client);
+
+            $this->assertNull(
+                $em
+                    ->getRepository(Client::class)
+                    ->findOneBy(
+                        [
+                            'identifier' => $client->getIdentifier(),
+                        ]
+                    )
+            );
+
+            $this->assertNull(
+                $em
+                    ->getRepository(AccessToken::class)
+                    ->findOneBy(
+                        [
+                            'identifier' => $accessToken->getIdentifier(),
+                        ]
+                    )
+            );
+        } finally {
+            timecop_return();
+        }
+    }
+
+    public function testClientDeleteCascadesToAccessTokensAndRefreshTokens(): void
+    {
+        /** @var $em EntityManagerInterface */
+        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $doctrineAccessTokenManager = new DoctrineClientManager($em);
+
+        $client = new Client('client', 'secret');
+        $em->persist($client);
+        $em->flush();
+
+        timecop_freeze(new DateTime());
+
+        try {
+            $accessToken = new AccessToken('access token', (new DateTime())->modify('+1 day'), $client, $client->getIdentifier(), []);
+            $em->persist($accessToken);
+            $em->flush();
+
+            $refreshToken = new RefreshToken('refresh token', (new DateTime())->modify('+1 day'), $accessToken);
+            $em->persist($refreshToken);
+            $em->flush();
+
+            $doctrineAccessTokenManager->remove($client);
+
+            $this->assertNull(
+                $em
+                    ->getRepository(Client::class)
+                    ->findOneBy(
+                        [
+                            'identifier' => $client->getIdentifier(),
+                        ]
+                    )
+            );
+
+            $this->assertNull(
+                $em
+                    ->getRepository(AccessToken::class)
+                    ->findOneBy(
+                        [
+                            'identifier' => $accessToken->getIdentifier(),
+                        ]
+                    )
+            );
+
+            $em->clear();
+
+            /** @var $refreshToken RefreshToken */
+            $refreshToken = $em
+                ->getRepository(RefreshToken::class)
+                ->findOneBy(
+                    [
+                        'identifier' => $refreshToken->getIdentifier(),
+                    ]
+                )
+            ;
+            $this->assertNotNull($refreshToken);
+            $this->assertNull($refreshToken->getAccessToken());
+        } finally {
+            timecop_return();
+        }
+    }
+}

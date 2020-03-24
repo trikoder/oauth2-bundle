d56dfb9fa37c4f37f6db70fb09c07e1f12682079
Toni Peric <thetoniperic@gmail.com>
2020-03-24 15:36:31 +0100

    Extract public API for finding clients

--- Manager/ClientManagerInterface.php
+++ Manager/ClientManagerInterface.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Trikoder\Bundle\OAuth2Bundle\Manager;
 
 use Trikoder\Bundle\OAuth2Bundle\Model\Client;
+use Trikoder\Bundle\OAuth2Bundle\Service\ClientFinderInterface;
 
-interface ClientManagerInterface
+interface ClientManagerInterface extends ClientFinderInterface
 {
-    public function find(string $identifier): ?Client;
-
     public function save(Client $client): void;
 
     public function remove(Client $client): void;

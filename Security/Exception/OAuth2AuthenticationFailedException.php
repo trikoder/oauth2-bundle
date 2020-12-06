c5f13918f157016c5f51efe8698da6cdbd2dbd87
Benoit VIGNAL <benoitvignal@gmail.com>
2020-12-06 18:25:24 +0100

    ADD: Create the AUTHENTICATION_FAILURE event

---
+++ Security/Exception/OAuth2AuthenticationFailedException.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Trikoder\Bundle\OAuth2Bundle\Security\Exception;
+
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ * @author Benoit VIGNAL <github@benoit-vignal.fr>
+ */
+class OAuth2AuthenticationFailedException extends AuthenticationException
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getMessageKey(): string {
+        return "OAuth Token not found";
+    }
+}

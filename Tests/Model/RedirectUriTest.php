8ddacdb8924a012cee636c8bcb3061b70617a704
Anthony Matignon <anthony.matignon@codes-rousseau.fr>
2021-03-09 11:10:14 +0100

    Add tests about Optional URI
    Add test and split failing tests
    
    Update assert on RedirectUri construct

--- Tests/Model/RedirectUriTest.php
+++ Tests/Model/RedirectUriTest.php
@@ -10,27 +10,44 @@ use Trikoder\Bundle\OAuth2Bundle\Model\RedirectUri;
 
 class RedirectUriTest extends TestCase
 {
-    public function testUris()
+    public function testPassingUris()
     {
         $passingUris = [
             'http://www.oauth2.com/redirect/to-uri',
             'https://oauth2.com/redirect-to-uri',
             'com.example.test123:/redirect/to-uri',
             'com.example.test:/redirect-to-uri',
-        ];
-
-        $faillingUris = [
-            'someRandomString',
+            'com.example.test:',
+            'http://www.oauth2.com',
         ];
 
         foreach ($passingUris as $uri) {
             $redirectUri = new RedirectUri($uri);
             $this->assertInstanceOf(RedirectUri::class, $redirectUri);
         }
+    }
 
-        foreach ($faillingUris as $uri) {
-            $this->expectException(RuntimeException::class);
-            $redirectUri = new RedirectUri($uri);
-        }
+    public function testFailingUri1()
+    {
+        $this->expectException(RuntimeException::class);
+        $redirectUri = new RedirectUri('comexampletest:/redirect/to-uri');
+    }
+
+    public function testFailingUri2()
+    {
+        $this->expectException(RuntimeException::class);
+        $redirectUri = new RedirectUri('someRandomString');
+    }
+
+    public function testFailingUri3()
+    {
+        $this->expectException(RuntimeException::class);
+        $redirectUri = new RedirectUri('.com.example.test:/redirect/to-uri');
+    }
+
+    public function testFailingUri4()
+    {
+        $this->expectException(RuntimeException::class);
+        $redirectUri = new RedirectUri('com.example.test.:/redirect/to-uri');
     }
 }
